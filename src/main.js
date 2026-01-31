e01b6fbb5f29216eefd653686ba65a232b8bb3be
G. Spencer Barkman <gbarkman@ucsc.edu>
2026-01-30 19:02:26 -0800

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+// G. Spencer Barkman
+// Super Rocket Patrol
+// Took (2) Hours
+// New Enemy Spaceship type (5 points)
+// 4 New Explosion SFX (3 points)
+// * Display the time remaining (in seconds) on the screen (3)
+// Music to Play Scene (1 point)
+// Speed increase after 30 seconds(1)
+// Randomize spaceship's direction (1)
+// * Allow the player to control the Rocket after it's fired (1) 
+// Implement a new timing/scoring mechanism that adds time to the clock for successful hits and subtracts time for misses (5)
+
+
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ]
+}
+
+let game = new Phaser.Game(config) 
+
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT
+
+//setting UI sizes
+let borderUISize = game.config.height/15
+let borderPadding = borderUISize/3
